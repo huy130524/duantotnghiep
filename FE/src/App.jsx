1826f3ce9bdaf4c4ae421a5ae6c943756b6ab8a4
Huy <huyv76984@gmail.com>
2025-02-20 21:32:54 +0700

    feat/productdeails

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -2,6 +2,7 @@ import HomePage from "./Pages/HomePage";
 import { Route, Routes } from "react-router-dom";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
+import Productdetails from "./Pages/ProductDetails";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route index element={<HomePage />} />
           <Route path="login" element={<Login />} />
+          <Route path="details" element={<Productdetails />} />
           <Route path="register" element={<Register />} />
         </Routes>
       </div>
