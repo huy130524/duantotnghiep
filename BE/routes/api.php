0e36894c1ccfe3d9f982de9e2592c315d3a73f9f
khanhprox <khanhvdph37690@fpt.edu.vn>
2025-02-24 21:31:03 +0700

    laravel

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -7,4 +7,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/products',[ProductController::class,'index']);
+Route::get('/products',[ProductController::class,'index']) ;
