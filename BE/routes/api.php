79f0163258326acc8f39390543a2a81d188b41ae
quangph39611 <anhvipbqhg@gmail.com>
2025-03-10 00:05:14 +0700

    API Product

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -11,4 +12,11 @@ Route::post('/category/add', [CategoryController::class,'create']);
 Route::get('/category/delete/{id}', [CategoryController::class,'deleteCategory']);
 Route::get('/category/detail/{id}', [CategoryController::class,'edit']);
 Route::post('/category/update/{id}', [CategoryController::class,'update']);
+
+// Product
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/product/add', [ProductController::class, 'store']);
+Route::get('/product/detail/{id}', [ProductController::class, 'ProductDetail']);
+Route::post('/product/update/{id}', [ProductController::class, 'update']);
 
