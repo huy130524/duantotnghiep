debd8a6c5aec118c2cc09f53fe7533ab156c2ac2
quangph39611 <anhvipbqhg@gmail.com>
2025-03-10 17:06:31 +0700

    API Màu sắc

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ColorController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -9,3 +10,11 @@ Route::post('/create-category', [CategoryController::class,'create']);
 Route::get('/delete-category/{id}', [CategoryController::class,'deleteCategory']);
 Route::get('/edit-category/{id}', [CategoryController::class,'edit']);
 Route::put('/edit-category/{id}', [CategoryController::class,'update']);
+
+
+// Color 
+
+Route::get('/colors', [ColorController::class, 'index']);
+Route::post('/color/add', [ColorController::class, 'store']);
+Route::get('/color/detail/{id}', [ColorController::class, 'detail']);
+Route::post('/color/update/{id}', [ColorController::class, 'update']);
