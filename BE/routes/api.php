a418eab755f020767ff987589125ff424f25fad5
quangph39611 <anhvipbqhg@gmail.com>
2025-03-10 16:40:23 +0700

    Route APi

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -9,3 +9,17 @@ Route::post('/create-category', [CategoryController::class,'create']);
 Route::get('/delete-category/{id}', [CategoryController::class,'deleteCategory']);
 Route::get('/edit-category/{id}', [CategoryController::class,'edit']);
 Route::put('/edit-category/{id}', [CategoryController::class,'update']);
+
+
+
+// Đăng ký đăng nhập
+
+
+Route::post('/change-password/{id}', [UserController::class, 'changePassword']);
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [UserController::class, 'logout']);
+
+
+Route::get('profile/{id}',[UserController::class,'profile']);
+Route::post('updateprofile/{id}',[UserController::class,'UpdateProfile']);
