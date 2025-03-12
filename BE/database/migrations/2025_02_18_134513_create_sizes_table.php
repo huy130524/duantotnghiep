e445ba05883ee19176454488df7d303de0538a77
quangph39611 <anhvipbqhg@gmail.com>
2025-03-12 15:23:33 +0700

    Sửa db bảng color và bảng size

--- BE/database/migrations/2025_02_18_134513_create_sizes_table.php
+++ BE/database/migrations/2025_02_18_134513_create_sizes_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('sizes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('color_code')->nullable();
+          
             $table->timestamps();
             $table->softDeletes();
         });
