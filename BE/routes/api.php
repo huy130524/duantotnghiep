52b3d39a1dd9cba2b1aac22c0ed27faa54b66a66
Hà Văn Tú <164178096+havantu2501@users.noreply.github.com>
2025-03-27 23:52:37 +0700

    crud my address

--- BE/routes/api.php
+++ BE/routes/api.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AddressController;
 
 use Illuminate\Support\Facades\Route;
 
--- FE/src/Pages/client/Profile/Address/AddressItem.jsx
+++ FE/src/Pages/client/Profile/Address/AddressItem.jsx
@@ -1,37 +1,79 @@
-import { Button } from "antd";
+import { message, Popconfirm } from "antd";
+import { api } from "../../../../api/api";
+import { useMutation } from "@tanstack/react-query";
+import UpdateAddressModal from "./UpdateAddressModal";
+import { useState } from "react";
 
 const AddressItem = (props) => {
-  const { isDefault } = props;
+  const { isDefault, data, refetch } = props;
+
+  const [openUpdateModal, setOpenUpdateModal] = useState(false);
+
+  const deleteAddressMutation = useMutation({
+    mutationKey: ["DELETE_ADDRESS"],
+    mutationFn: () => api.delete(`/addresses/delete/${data?.id}`),
+    onSuccess: () => {
+      message.success("Xoá địa chỉ thành công");
+      refetch();
+    },
+    onError: (error) => {
+      message.error(error?.response?.data?.message || "Có lỗi xảy ra");
+    },
+  });
 
   return (
-    <div className="tw-py-5 tw-border-t tw-border-x-0 tw-border-b-0 tw-border-solid tw-border-[#cfcfcf] tw-flex tw-items-center tw-justify-between tw-gap-x-4">
-      <div className="tw-text-[14px] tw-flex-1">
-        <div className="tw-flex tw-items-center tw-gap-x-2">
-          {isDefault && (
-            <p className="tw-border tw-border-[#e30019] tw-border-solid tw-rounded tw-h-7 tw-px-2 tw-text-[#e30019] tw-flex tw-items-center">
-              Mặc định
+    <>
+      <div className="tw-py-5 tw-border-t tw-border-x-0 tw-border-b-0 tw-border-solid tw-border-[#cfcfcf] tw-flex tw-items-center tw-justify-between tw-gap-x-4">
+        <div className="tw-text-[14px] tw-flex-1">
+          <div className="tw-flex tw-items-center tw-gap-x-2">
+            {isDefault && (
+              <p className="tw-border tw-border-[#e30019] tw-m-0 tw-border-solid tw-rounded tw-h-7 tw-px-2 tw-text-[#e30019] tw-flex tw-items-center">
+                Mặc định
+              </p>
+            )}
+
+            {/* <p className="tw-text-[#111] tw-font-semibold">Hà Văn Tú</p>
+          <p className="tw-text-[#535353]">|</p> */}
+            <p className="tw-text-[#535353] tw-m-0">{data?.phone}</p>
+          </div>
+
+          <p className="tw-text-[#535353] tw-mt-2 tw-m-0">
+            {`${data?.street}, ${data?.district}, ${data?.city}`}
+          </p>
+        </div>
+
+        <div>
+          <div className="tw-flex tw-items-center tw-gap-x-3 tw-justify-end tw-text-[14px] tw-mb-2 tw-text-[#1982f9]">
+            <p
+              className="tw-cursor-pointer tw-m-0"
+              onClick={() => setOpenUpdateModal(true)}
+            >
+              Cập nhật
             </p>
-          )}
+            {!isDefault && (
+              <Popconfirm
+                title="Xoá địa chỉ"
+                description="Bạn có chắc chắn muốn xoá địa chỉ này?"
+                okText="Xoá"
+                cancelText="Hủy"
+                onConfirm={deleteAddressMutation.mutate}
+              >
+                <p className="tw-cursor-pointer tw-m-0">Xoá</p>
+              </Popconfirm>
+            )}
+          </div>
 
-          <p className="tw-text-[#111] tw-font-semibold">Hà Văn Tú</p>
-          <p className="tw-text-[#535353]">|</p>
-          <p className="tw-text-[#535353]">0983983983</p>
+          {/* {!isDefault && <Button>Thiết lập mặc định</Button>} */}
         </div>
-
-        <p className="tw-text-[#535353] tw-mt-2">
-          ĐƠN TEST, Xã An Phú Tây, Huyện Bình Chánh, Hồ Chí Minh, Vietnam
-        </p>
       </div>
 
-      <div>
-        <div className="tw-flex tw-items-center tw-gap-x-3 tw-justify-end tw-text-[14px] tw-mb-2 tw-text-[#1982f9]">
-          <p className="tw-cursor-pointer">Cập nhật</p>
-          {!isDefault && <p className="tw-cursor-pointer">Xoá</p>}
-        </div>
-
-        {!isDefault && <Button>Thiết lập mặc định</Button>}
-      </div>
-    </div>
+      <UpdateAddressModal
+        open={openUpdateModal}
+        onClose={() => setOpenUpdateModal(false)}
+        onSuccess={refetch}
+        id={data?.id}
+      />
+    </>
   );
 };
 
